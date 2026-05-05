a15ae3a7c05136a048cf2a9ba4973ce1203a11cf
Arman Shrivastava <ashrivastava@geduservices.com>
2026-05-05 13:34:24 +0530

    pipeline ready with all componenets

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup , find_packages
+# this is the setup file for the MLFlow Experiment Tracking project. It defines the package metadata and dependencies.
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Arman Shrivastava",
+    packages=find_packages()
+)
+
+#this file is used to package the project and make it installable. It specifies the name, version, description, and packages to include in the distribution.
