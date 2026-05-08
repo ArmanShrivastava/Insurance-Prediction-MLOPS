196904c976b8d2a641d30840b72f1bfdd0eb0d95
Arman Shrivastava <armanshrivastava@Armans-MacBook-Air.local>
2026-05-08 20:34:47 +0530

    application setup locally

--- src/entity/estimator.py
+++ src/entity/estimator.py
@@ -5,3 +5,12 @@ from dataclasses import dataclass
 class MyModel:
     preprocessing_object: object
     trained_model_object: object
+    
+    def predict(self, dataframe):
+        """
+        Preprocess the dataframe and make predictions using the trained model
+        :param dataframe: Input dataframe for prediction
+        :return: Predictions from the trained model
+        """
+        processed_dataframe = self.preprocessing_object.transform(dataframe)
+        return self.trained_model_object.predict(processed_dataframe)
